b8f0011c1dee578369a7a11ecf8bd40090d1e8fb
Phideon <a2z0to9@outlook.com>
2020-03-22 20:42:07 +0800

    feat: init.

---
+++ src/main/java/com/lee/datastructureandalgorithms/DataStructureAndAlgorithmsApplication.java
@@ -0,0 +1,17 @@
+package com.lee.datastructureandalgorithms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author i324779
+ */
+@SpringBootApplication
+public class DataStructureAndAlgorithmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataStructureAndAlgorithmsApplication.class, args);
+        System.out.println("leetcode");
+    }
+
+}
