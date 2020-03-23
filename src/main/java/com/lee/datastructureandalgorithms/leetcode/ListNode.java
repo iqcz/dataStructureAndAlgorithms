6663a7a3c60ed0444ca35af373a61fb944a93d7b
Phideon <a2z0to9@outlook.com>
2020-03-23 12:26:10 +0800

    fixed:链表两数相加

---
+++ src/main/java/com/lee/datastructureandalgorithms/leetcode/ListNode.java
@@ -0,0 +1,31 @@
+package com.lee.datastructureandalgorithms.leetcode;
+
+public class ListNode {
+
+    private int data;
+
+    private ListNode next;
+
+    public ListNode() {
+    }
+
+    public ListNode(int data) {
+        this.data = data;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public ListNode getNext() {
+        return next;
+    }
+
+    public void setNext(ListNode next) {
+        this.next = next;
+    }
+}
