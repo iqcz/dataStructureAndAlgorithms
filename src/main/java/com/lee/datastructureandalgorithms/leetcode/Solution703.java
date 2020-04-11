d1108c209f7a7d505bb11a4155aedef996e66e6f
Phideon <a2z0to9@outlook.com>
2020-04-12 00:02:27 +0800

    fixed:703 实现

--- src/main/java/com/lee/datastructureandalgorithms/leetcode/Solution703.java
+++ src/main/java/com/lee/datastructureandalgorithms/leetcode/Solution703.java
@@ -1,12 +1,14 @@
 package com.lee.datastructureandalgorithms.leetcode;
 
+import java.util.PriorityQueue;
+
 /**
  * 设计一个找到数据流中第K大元素的类（class）。注意是排序后的第K大元素，不是第K个不同的元素。
- *
+ * <p>
  * 你的 KthLargest 类需要一个同时接收整数 k 和整数数组nums 的构造器，它包含数据流中的初始元素。每次调用 KthLargest.add，返回当前数据流中第K大的元素。
- *
+ * <p>
  * 示例:
- *
+ * <p>
  * int k = 3;
  * int[] arr = [4,5,8,2];
  * KthLargest kthLargest = new KthLargest(3, arr);
@@ -17,8 +19,35 @@ package com.lee.datastructureandalgorithms.leetcode;
  * kthLargest.add(4);   // returns 8
  * 说明:
  * 你可以假设 nums 的长度≥ k-1 且k ≥ 1。
- *
  */
 public class Solution703 {
-    
+
+    public static void main(String[] args) {
+        int[] a = {3, 4, 1, 9, 5, 6};
+        KthLargest kthLargest = new KthLargest(3,a);
+        System.out.println(kthLargest.add(8));
+    }
+}
+
+class KthLargest {
+    final PriorityQueue<Integer> queue;
+    final int k;
+
+    public KthLargest(int k, int[] a) {
+        this.k = k;
+        queue = new PriorityQueue<>(k);
+        for (int n : a) {
+            add(n);
+        }
+    }
+
+    public int add(int n) {
+        if (queue.size() < k) {
+            queue.offer(n);
+        } else if (queue.peek() < n) {
+            queue.poll();
+            queue.offer(n);
+        }
+        return queue.peek();
+    }
 }
