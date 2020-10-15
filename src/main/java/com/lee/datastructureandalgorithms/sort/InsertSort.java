21f5c87cdce58031fae29de4e6d2be93de8aaf9f
Phideon <a2z0to9@outlook.com>
2020-10-16 07:47:20 +0800

    feat: insert and shell sort

--- src/main/java/com/lee/datastructureandalgorithms/sort/InsertSort.java
+++ src/main/java/com/lee/datastructureandalgorithms/sort/InsertSort.java
@@ -20,9 +20,25 @@ public class InsertSort {
         }
     }
 
+    public static void insertSort2(int[] a) {
+        int length = a.length;
+        for (int i = 1; i < length; i++) {
+            int data = a[i];
+            for (int j = i - 1; j >= 0; j--) {
+                if (a[j] > data) {
+                    int tmp = a[j + 1];
+                    a[j + 1] = a[j];
+                    a[j] = tmp;
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) {
         int[] a = {3, 2, 5, 99, 34, 21, 98};
-        insertSort(a);
+        insertSort2(a);
         for (int i : a) {
             System.out.printf("%d\t", i);
         }
