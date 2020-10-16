c4a16ef6d1cf9301ab6ceaa66de453e4dcf617aa
Phideon <a2z0to9@outlook.com>
2020-10-16 08:30:04 +0800

    feat:merge sort

--- src/main/java/com/lee/datastructureandalgorithms/leetcode/DDLNode.java
+++ src/main/java/com/lee/datastructureandalgorithms/leetcode/DDLNode.java
@@ -1,2 +1,42 @@
-package com.lee.datastructureandalgorithms.leetcode;public class DDLNode {
+package com.lee.datastructureandalgorithms.leetcode;
+
+/**
+ * 双向链表结构定义
+ * @author i324779
+ */
+public class DDLNode {
+    private int data;
+
+    private DDLNode previous;
+
+    private DDLNode next;
+
+    public DDLNode() {
+
+    }
+
+    public DDLNode(int data) {
+        this.data = data;
+    }
+
+
+    public int getData() {
+        return data;
+    }
+
+    public DDLNode getPrevious() {
+        return previous;
+    }
+
+    public void setPrevious(DDLNode previous) {
+        this.previous = previous;
+    }
+
+    public DDLNode getNext() {
+        return next;
+    }
+
+    public void setNext(DDLNode next) {
+        this.next = next;
+    }
 }
