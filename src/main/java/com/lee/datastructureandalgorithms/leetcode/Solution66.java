45a68cb7fad60f8fa0851b5072c621b302bd6141
HyperFocus <a2z0to9@outlook.com>
2020-11-03 08:11:03 +0800

    feat:array

---
+++ src/main/java/com/lee/datastructureandalgorithms/leetcode/Solution66.java
@@ -0,0 +1,49 @@
+package com.lee.datastructureandalgorithms.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * @author osyuj
+ */
+public class Solution66 {
+
+    public int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] != 9) {
+                digits[i]++;
+                return digits;
+            } else {
+                digits[i] = 0;
+            }
+        }
+
+        digits = new int[digits.length + 1];
+        digits[0] = 1;
+        return digits;
+    }
+
+    public int[] plusOne1(int[] digits) {
+        // 1999
+        // 1001
+        // 999
+        for (int i = digits.length - 1; i >= 0; i--) {
+            digits[i]++;
+            digits[i] = digits[i] % 10;
+            if (digits[i] != 0) {
+                return digits;
+            }
+        }
+        digits = new int[digits.length + 1];
+        digits[0] = 1;
+        return digits;
+    }
+
+    public static void main(String[] args) {
+//        int[] digits = {1, 2, 3};
+//        int[] digits = {1, 2, 3};
+        int[] digits = {8, 9, 9};
+        Solution66 solution66 = new Solution66();
+        System.out.println(Arrays.toString(solution66.plusOne(digits)));
+
+    }
+}
