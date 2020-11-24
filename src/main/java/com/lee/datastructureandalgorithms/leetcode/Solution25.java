2c2e416f9e78446c252b7e6b0c63d289bc4b72db
HyperFocus <a2z0to9@outlook.com>
2020-11-24 23:50:43 +0800

    feat:25

---
+++ src/main/java/com/lee/datastructureandalgorithms/leetcode/Solution25.java
@@ -0,0 +1,44 @@
+package com.lee.datastructureandalgorithms.leetcode;
+
+/**
+ * @author osyuj
+ */
+public class Solution25 {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode dummy = new ListNode(0);
+        dummy.setNext(head);
+
+        ListNode pre = dummy;
+        ListNode end = dummy;
+
+        while (end.getNext() != null) {
+            for (int i = 0; i < k && end != null; i++) {
+                end = end.getNext();
+            }
+            if (end == null) {
+                break;
+            }
+            ListNode start = pre.getNext();
+            ListNode next = end.getNext();
+            end.setNext(null);
+            pre.setNext(reverse(start));
+            start.setNext(next);
+            pre = start;
+
+            end = pre;
+        }
+        return dummy.getNext();
+    }
+
+    private ListNode reverse(ListNode head) {
+        ListNode pre = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode next = curr.getNext();
+            curr.setNext(pre);
+            pre = curr;
+            curr = next;
+        }
+        return pre;
+    }
+}
