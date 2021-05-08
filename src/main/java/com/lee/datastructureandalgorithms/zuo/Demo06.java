83e966a1b9f60287209a3a6d1188d6ea4e0cee67
Phideon <a2z0to9@outlook.com>
2021-05-08 09:15:13 +0800

    feat:maximum depth of binary tree

---
+++ src/main/java/com/lee/datastructureandalgorithms/zuo/Demo06.java
@@ -0,0 +1,14 @@
+package com.lee.datastructureandalgorithms.zuo;
+
+/**
+ * maximum depth of binary tree
+ */
+public class Demo06 {
+
+    public static int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+}
