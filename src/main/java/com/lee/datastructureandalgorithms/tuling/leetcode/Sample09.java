1cb0f4739f8097a55fec35b8582fbb5e01e50dfd
HyperFocus <a2z0to9@outlook.com>
2021-09-04 17:38:21 +0800

    feat:code

---
+++ src/main/java/com/lee/datastructureandalgorithms/tuling/leetcode/Sample09.java
@@ -0,0 +1,22 @@
+package com.lee.datastructureandalgorithms.tuling.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * 一个整型数组 nums ，在数组中找出由三个数字组成的最大乘积，并输出这个乘积。
+ *
+ * @author osyuj
+ */
+public class Sample09 {
+    public static void main(String[] args) {
+        int[] nums = {-9, -5, 2, 4, 7};
+        System.out.println("maxBySort(nums) = " + maxBySort(nums));
+    }
+    
+    public static int maxBySort(int[] nums) {
+        Arrays.sort(nums);
+        int length = nums.length;
+        return Math.max(nums[0] * nums[1] * nums[length - 1],
+                nums[length - 1] * nums[length - 2] * nums[length - 3]);
+    }
+}
