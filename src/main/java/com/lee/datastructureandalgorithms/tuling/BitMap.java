58e10ec43cf9ec5ff353ca025a0306b6d4dd0f6d
HyperFocus <a2z0to9@outlook.com>
2021-09-30 21:28:17 +0800

    feat:双指针

--- src/main/java/com/lee/datastructureandalgorithms/tuling/BitMap.java
+++ src/main/java/com/lee/datastructureandalgorithms/tuling/BitMap.java
@@ -34,7 +34,6 @@ public class BitMap {
         int loc = n % 8;
         //接下来就是要把bit数组里面的 bisIndex这个下标的byte里面的 第loc个bit位置为1
         bits[bitsIndex] |= 1 << loc;
-
     }
 
     public boolean find(int n) {
@@ -59,4 +58,3 @@ public class BitMap {
         System.out.println(bitMap.find(64));
     }
 }
-
