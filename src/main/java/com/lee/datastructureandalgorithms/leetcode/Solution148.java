ff8378dd1b7c1a9ce49d24938f7a7ef342c39ace
HyperFocus <a2z0to9@outlook.com>
2021-11-07 16:36:30 +0800

    feat: leetcode

---
+++ src/main/java/com/lee/datastructureandalgorithms/leetcode/Solution148.java
@@ -0,0 +1,44 @@
+package com.lee.datastructureandalgorithms.leetcode;
+
+public class Solution141 {
+    public boolean hasCycle(ListNode head) {
+        if (head == null) {
+            return false;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast.getNext() != null) {
+            fast = fast.getNext();
+            if (fast.getNext() == null) {
+                return false;
+            }
+            fast = fast.getNext();
+            slow = slow.getNext();
+
+            if (fast == slow) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean hasCycle1(ListNode head) {
+        if (head == null || head.getNext() == null) {
+            return false;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head.getNext();
+        while (slow != fast) {
+            if (fast == null || fast.getNext() == null) {
+                return false;
+            }
+
+            slow = slow.getNext();
+            fast = fast.getNext().getNext();
+        }
+        return true;
+    }
+}
