eaeae8f69daaecd259726f8f680342665032c8b7
HyperFocus <a2z0to9@outlook.com>
2021-11-07 16:13:08 +0800

    feat:leetcode 24

---
+++ src/main/java/com/lee/datastructureandalgorithms/leetcode/Solution24.java
@@ -0,0 +1,21 @@
+package com.lee.datastructureandalgorithms.leetcode;
+
+/**
+ * @author osyuj
+ */
+public class Solution24 {
+    public ListNode swapPairs(ListNode head) {
+        ListNode sentinel = new ListNode(0);
+        sentinel.setNext(head);
+        ListNode current = sentinel;
+        while (current.getNext() != null && current.getNext().getNext() != null) {
+            ListNode node1 = current.getNext();
+            ListNode node2 = current.getNext().getNext();
+            current.setNext(node2);
+            node1.setNext(node2.getNext());
+            node2.setNext(node1);
+            current = node1;
+        }
+        return sentinel.getNext();
+    }
+}
