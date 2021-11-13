c7a0d7f7215d6e9f25dc9fca96df7c1c2ed8c4be
HyperFocus <a2z0to9@outlook.com>
2021-11-13 20:56:22 +0800

    docs:comment

--- src/main/java/com/lee/datastructureandalgorithms/leetcode/ObjectSize.java
+++ src/main/java/com/lee/datastructureandalgorithms/leetcode/ObjectSize.java
@@ -15,13 +15,13 @@ public class ObjectSize {
 //        System.out.println("MyObject对象内存");
 //        ClassLayout layout2 = ClassLayout.parseInstance(new MyObject());
 //        System.out.println(layout2.toPrintable());
-    }
+//    }
 
     /**
      * ‐XX:+UseCompressedOops 默认开启的压缩所有指针
      * ‐XX:+UseCompressedClassPointers 默认开启的压缩对象头里的类型指针Klass Pointer
      * Oops : Ordinary Object Pointers
-     */
+
     public static class MyObject {
         //8B mark word
         //4B Klass Pointer 如果关闭压缩‐XX:‐UseCompressedClassPointers或‐XX:‐UseCompressedOops，则占用8B
@@ -30,4 +30,5 @@ public class ObjectSize {
         byte b; //1B
         Object o; //4B 如果关闭压缩‐XX:‐UseCompressedOops，则占用8B
     }
+     */
 }
