e829d0fb5a336bfd3421b9141d57501ca963aa6d
Phideon <a2z0to9@outlook.com>
2021-12-16 13:58:53 +0800

    feat:test

---
+++ src/main/java/com/lee/datastructureandalgorithms/yu/TheadDemo.java
@@ -0,0 +1,11 @@
+package com.lee.datastructureandalgorithms.yu;
+
+/**
+ * 多线程实现奇数偶数间隔打印，例如线程1打印奇数，线程2打印偶数，间隔打印。
+ * @author i324779
+ */
+public class TheadDemo {
+    public static void main(String[] args) {
+
+    }
+}
