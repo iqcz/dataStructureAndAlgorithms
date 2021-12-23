d1a89cbe6211453757dd8b4a57285ce81f4fe2fd
HyperFocus <a2z0to9@outlook.com>
2021-12-23 23:49:18 +0800

    feat: quesetion24

---
+++ src/main/java/com/lee/datastructureandalgorithms/offer/Question24.java
@@ -0,0 +1,26 @@
+package com.lee.datastructureandalgorithms.offer;
+
+import com.lee.datastructureandalgorithms.leetcode.ListNode;
+
+/**
+ * @author D49450
+ */
+public class Question24 {
+    public ListNode reverseList(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+
+        ListNode current = head;
+        ListNode previous = null;
+        ListNode next;
+        while (current != null) {
+            next = current.getNext();
+            current.setNext(previous);
+            previous = current;
+            current = next;
+        }
+
+        return previous;
+    }
+}
