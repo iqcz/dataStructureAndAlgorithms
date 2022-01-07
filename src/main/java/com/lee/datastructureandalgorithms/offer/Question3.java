b2b0e1abbfd7a169354e88bcc3fa061e3985ad37
Phideon <a2z0to9@outlook.com>
2022-01-07 22:34:26 +0800

    feat:question3

--- src/main/java/com/lee/datastructureandalgorithms/offer/Question3.java
+++ src/main/java/com/lee/datastructureandalgorithms/offer/Question3.java
@@ -3,6 +3,7 @@ package com.lee.datastructureandalgorithms.offer;
 import java.util.Arrays;
 
 /**
+ * 前 n 个数字二进制形式中1的个数
  * @author D49450
  */
 public class Question3 {
@@ -21,8 +22,18 @@ public class Question3 {
         return result;
     }
 
+    public static int[] countBits2(int num) {
+        int[] result = new int[num + 1];
+        for (int i = 1; i <= num; i++) {
+            result [i] = result[i & (i - 1)] + 1;
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
         int[] result  = countBits(4);
         System.out.println("Arrays.toString(result) = " + Arrays.toString(result));
+        int[] result1 = countBits2(4);
+        System.out.println("Arrays.toString(result1) = " + Arrays.toString(result1));
     }
 }
