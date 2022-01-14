dcec4d89bee54ebe8855934f2bb0d33eae8e96e1
Phideon <a2z0to9@outlook.com>
2022-01-14 23:18:17 +0800

    feat: solution283

--- src/main/java/com/lee/datastructureandalgorithms/leetcode/Solution283.java
+++ src/main/java/com/lee/datastructureandalgorithms/leetcode/Solution283.java
@@ -2,6 +2,7 @@ package com.lee.datastructureandalgorithms.leetcode;
 
 /**
  * 给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+ *
  * @author D49450
  */
 public class Solution283 {
@@ -25,5 +26,21 @@ public class Solution283 {
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
+    }
+
+    public void moveZeroes1(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return;
+        }
+
+        int j = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[j++] = nums[i];
+            }
+        }
+        for (int i = j; j < nums.length; i++) {
+            nums[i] = 0;
+        }
     }
 }
