c84cc5f5900b20ec5c4b4f20f502f5c0b3a7da1a
HyperFocus <a2z0to9@outlook.com>
2022-03-09 23:10:29 +0800

    docs: add comments

--- src/main/java/com/lee/datastructureandalgorithms/offer/Question3.java
+++ src/main/java/com/lee/datastructureandalgorithms/offer/Question3.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 
 /**
  * 前 n 个数字二进制形式中1的个数
+ *
  * @author D49450
  */
 public class Question3 {
@@ -16,6 +17,7 @@ public class Question3 {
             while (j != 0) {
                 result[i]++;
                 // 计算i的二进制形式中1的个数
+                // 记住了就行，该记忆就要记忆
                 j = j & (j - 1);
             }
         }
@@ -25,13 +27,13 @@ public class Question3 {
     public static int[] countBits2(int num) {
         int[] result = new int[num + 1];
         for (int i = 1; i <= num; i++) {
-            result [i] = result[i & (i - 1)] + 1;
+            result[i] = result[i & (i - 1)] + 1;
         }
         return result;
     }
 
     public static void main(String[] args) {
-        int[] result  = countBits(4);
+        int[] result = countBits(4);
         System.out.println("Arrays.toString(result) = " + Arrays.toString(result));
         int[] result1 = countBits2(4);
         System.out.println("Arrays.toString(result1) = " + Arrays.toString(result1));
