29c715f249c96c6f0c63667605fd7ceae5a1677b
HyperFocus <a2z0to9@outlook.com>
2022-03-23 21:39:53 +0800

    feat: leetcode listNode

---
+++ src/main/java/com/lee/datastructureandalgorithms/leetcode/LeeCodeListNode.java
@@ -0,0 +1,22 @@
+package com.lee.datastructureandalgorithms.leetcode;
+
+/**
+ * @author D49450
+ */
+public class LeeCodeListNode {
+    public int val;
+    public LeeCodeListNode next;
+
+    public LeeCodeListNode() {
+    }
+
+    public LeeCodeListNode(int val) {
+        this.val = val;
+    }
+
+    public LeeCodeListNode(int val, LeeCodeListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
