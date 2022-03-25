0f3dccf427c7d7652d7ed904de228b2b32e277bf
HyperFocus <a2z0to9@outlook.com>
2022-03-25 23:29:59 +0800

    feat: merge tree node.

--- src/main/java/com/lee/datastructureandalgorithms/leetcode/Solution617.java
+++ src/main/java/com/lee/datastructureandalgorithms/leetcode/Solution617.java
@@ -5,7 +5,16 @@ package com.lee.datastructureandalgorithms.leetcode;
  */
 public class Solution617 {
     public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        if (root1 == null) {
+            return root2;
+        }
+        if (root2 == null) {
+            return root1;
+        }
 
-        return null;
+        TreeNode mergedTree = new TreeNode(root1.val + root2.val);
+        mergedTree.left = this.mergeTrees(root1.left, root2.left);
+        mergedTree.right = this.mergeTrees(root1.right, root2.right);
+        return mergedTree;
     }
 }
