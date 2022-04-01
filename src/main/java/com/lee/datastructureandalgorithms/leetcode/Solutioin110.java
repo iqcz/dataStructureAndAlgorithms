52524f16fe101598ebdfc93fe2bb7e44511f4aeb
HyperFocus <a2z0to9@outlook.com>
2022-04-01 22:50:23 +0800

    feat:solution110

---
+++ src/main/java/com/lee/datastructureandalgorithms/leetcode/Solutioin110.java
@@ -0,0 +1,26 @@
+package com.lee.datastructureandalgorithms.leetcode;
+
+/**
+ * 给定一个二叉树，判断它是否是高度平衡的二叉树。
+ * 本题中，一棵高度平衡二叉树定义为：
+ * 一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1 。
+ *
+ * @author D49450
+ */
+public class Solutioin110 {
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return Math.abs(height(root.left) - height(root.right)) <= 1
+                && isBalanced(root.left)
+                && isBalanced(root.right);
+    }
+
+    public int height(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return Math.max(height(root.left), height(root.right)) + 1;
+    }
+}
