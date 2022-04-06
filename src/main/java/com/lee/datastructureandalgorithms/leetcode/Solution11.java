deac872e8dc8235197b03b42f1bfa3b2ff9f92fc
HyperFocus <a2z0to9@outlook.com>
2022-04-06 21:56:07 +0800

    feat: solution 11

---
+++ src/main/java/com/lee/datastructureandalgorithms/leetcode/Solution11.java
@@ -0,0 +1,17 @@
+package com.lee.datastructureandalgorithms.leetcode;
+
+/**
+ * @author D49450
+ */
+public class Solution11 {
+
+    public int maxArea(int[] height) {
+        int i = 0;
+        int j = height.length - 1;
+        int maxWater = 0;
+        while (i < j) {
+            maxWater = height[i] < height[j] ? Math.max(maxWater, (j - i) * height[i++]) : Math.max(maxWater, (j - i) * height[j--]);
+        }
+        return maxWater;
+    }
+}
