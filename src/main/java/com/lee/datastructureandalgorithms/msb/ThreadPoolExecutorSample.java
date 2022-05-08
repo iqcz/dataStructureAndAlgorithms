c1a9725f7eee574663c8ebe9b5faee8246ac782b
HyperFocus <a2z0to9@outlook.com>
2022-05-08 23:34:46 +0800

    fix: static

--- src/main/java/com/lee/datastructureandalgorithms/msb/ThreadPoolExecutorSample.java
+++ src/main/java/com/lee/datastructureandalgorithms/msb/ThreadPoolExecutorSample.java
@@ -9,11 +9,11 @@ import java.util.concurrent.TimeUnit;
 
 public class ThreadPoolExecutorSample {
 
-    private static final int count = 100;
+    private static final int QUEUE_SIZE = 100;
 
     public static void main(String[] args) {
         ThreadPoolExecutor tp = new ThreadPoolExecutor(4, 20, 60, TimeUnit.SECONDS,
-                new LinkedBlockingQueue<>(count));
+                new LinkedBlockingQueue<>(QUEUE_SIZE));
 
         List<String> names = Lists.newArrayList("1", "2", "3", "4", "5" ,"6" , "7","8", "9", "10" ,"11", "12", "13"
         , "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31");
