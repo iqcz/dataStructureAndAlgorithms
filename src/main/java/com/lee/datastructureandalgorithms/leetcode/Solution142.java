bbd412e3780c715de0fbded35575f91681c41a02
HyperFocus <a2z0to9@outlook.com>
2022-06-24 10:13:04 +0800

    refact: code

--- src/main/java/com/lee/datastructureandalgorithms/leetcode/Solution142.java
+++ src/main/java/com/lee/datastructureandalgorithms/leetcode/Solution142.java
@@ -11,6 +11,7 @@ public class Solution142 {
         ListNode slowPointer = head;
         ListNode fastPointer = head;
         boolean existsCycle = false;
+        // 找到环
         while (fastPointer.getNext() != null && fastPointer.getNext().getNext() != null) {
             slowPointer = slowPointer.getNext();
             fastPointer = fastPointer.getNext().getNext();
@@ -20,7 +21,9 @@ public class Solution142 {
             }
         }
 
+        // 存在环
         if (existsCycle) {
+            // 慢指针重新指向表头
             slowPointer = head;
             while (slowPointer != fastPointer) {
                 slowPointer = slowPointer.getNext();
