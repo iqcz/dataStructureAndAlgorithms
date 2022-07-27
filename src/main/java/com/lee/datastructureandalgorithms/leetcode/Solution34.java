eb15c82a5071837cf8ce6c4f78ba081b3715e8f9
HyperFocus <a2z0to9@outlook.com>
2022-07-27 21:50:39 +0800

    feat:solution34.

---
+++ src/main/java/com/lee/datastructureandalgorithms/leetcode/Solution34.java
@@ -0,0 +1,49 @@
+package com.lee.datastructureandalgorithms.leetcode;
+
+class Solution34 {
+    public int[] searchRange(int[] nums, int target) {
+        int[] result = new int[2];
+        result[0] = findFirst(nums, target);
+        if (result[0] == -1) {
+            return new int[]{-1, -1};
+        }
+        result[1] = findLast(result[0], nums, target);
+        return result;
+    }
+
+    private int findFirst(int[] nums, int target) {
+        int idx = -1;
+        int start = 0;
+        int end = nums.length - 1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (nums[mid] >= target) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+            if (nums[mid] == target) {
+                idx = mid;
+            }
+        }
+        return idx;
+    }
+
+    private int findLast(int first, int[] nums, int target) {
+        int idx = -1;
+        int start = first;
+        int end = nums.length - 1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (nums[mid] <= target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+            if (nums[mid] == target) {
+                idx = mid;
+            }
+        }
+        return idx;
+    }
+}
