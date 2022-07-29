c4bb0cd597ae7a40ac44709348e28c413488b839
HyperFocus <a2z0to9@outlook.com>
2022-07-30 06:55:37 +0800

    feat:solution124

---
+++ src/main/java/com/lee/datastructureandalgorithms/leetcode/Solution124.java
@@ -0,0 +1,23 @@
+package com.lee.datastructureandalgorithms.leetcode;
+
+public class Solution124 {
+    int maxValue = Integer.MIN_VALUE;
+
+    public int maxPathSum(TreeNode root) {
+        maxPathDown(root);
+        return maxValue;
+    }
+
+    private int maxPathDown(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+
+        int left = Math.max(0, maxPathDown(node.left));
+        int right = Math.max(0, maxPathDown(node.right));
+
+        maxValue = Math.max(maxValue, left + right + node.val);
+
+        return Math.max(left, right) + node.val;
+    }
+}
