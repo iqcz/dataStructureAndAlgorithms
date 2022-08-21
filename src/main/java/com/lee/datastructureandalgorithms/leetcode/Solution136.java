2bc294755634c719d8e755327846969ee69eee5a
HyperFocus <a2z0to9@outlook.com>
2022-08-21 17:33:49 +0800

    docs: add commens.

--- src/main/java/com/lee/datastructureandalgorithms/leetcode/Solution136.java
+++ src/main/java/com/lee/datastructureandalgorithms/leetcode/Solution136.java
@@ -2,19 +2,20 @@ package com.lee.datastructureandalgorithms.leetcode;
 
 /**
  * 只出现一次的数字
+ *
  * @author D49450
  */
 public class Solution136 {
     public static int singleNumber(int[] nums) {
         int ret = 0;
-        for(int num : nums) {
+        for (int num : nums) {
             ret ^= num;
         }
         return ret;
     }
 
     public static void main(String[] args) {
-        int [] data = {2, 2, 7, 4, 5, 4, 7};
+        int[] data = {2, 2, 7, 4, 5, 4, 7};
         System.out.println("singleNumber(data) = " + singleNumber(data));
 
         int a = 4 ^ 3 ^ 4 ^ 3 ^ 8;
