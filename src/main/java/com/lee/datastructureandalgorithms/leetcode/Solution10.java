4acc5b19bc397573c254fa51c4b3e68807a76b85
HyperFocus <a2z0to9@outlook.com>
2022-08-23 23:16:20 +0800

    feat: 整数转罗马数字

---
+++ src/main/java/com/lee/datastructureandalgorithms/leetcode/Solution10.java
@@ -0,0 +1,20 @@
+package com.lee.datastructureandalgorithms.leetcode;
+
+/**
+ * 整数转罗马数字
+ *
+ * @author D49450
+ */
+public class Solution10 {
+    String[] thousands = {"", "M", "MM", "MMM"};
+    String[] hundreds = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+    String[] tens = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+    String[] ones = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+
+    public String intToRoman(int num) {
+        return thousands[num / 1000]
+                + hundreds[num % 1000 / 100]
+                + tens[num % 100 / 10]
+                + ones[num % 10];
+    }
+}
