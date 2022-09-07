909b7f7a6bbfdc1d622e40ee02a27cf772648bc9
HyperFocus <a2z0to9@outlook.com>
2022-09-07 21:52:40 +0800

    feat; empty method.

--- src/main/java/com/lee/datastructureandalgorithms/leetcode/Solution17.java
+++ src/main/java/com/lee/datastructureandalgorithms/leetcode/Solution17.java
@@ -1,8 +1,13 @@
 package com.lee.datastructureandalgorithms.leetcode;
 
+import java.util.List;
+
 /**
  * 电话号码的字母组合
  * @author D49450
  */
 public class Solution17 {
+    public List<String> letterCombinations(String digits) {
+        return null;
+    }
 }
