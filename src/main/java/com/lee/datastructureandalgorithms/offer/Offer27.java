36815fb2e4e9ba5a6b8ce47387cb7fb0dfcd66b2
Phideon <a2z0to9@outlook.com>
2022-09-28 11:40:41 +0800

    feat: offer27

---
+++ src/main/java/com/lee/datastructureandalgorithms/offer/Offer27.java
@@ -0,0 +1,27 @@
+package com.lee.datastructureandalgorithms.offer;
+
+import com.lee.datastructureandalgorithms.leetcode.TreeNode;
+
+/**
+ * 完成一个函数，输入一个二叉树，该函数输出它的镜像。
+ *
+ * @author i324779
+ */
+public class Offer27 {
+    public TreeNode mirrorTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+
+        mirrorTree(root.left);
+        mirrorTree(root.right);
+        swap(root);
+        return root;
+    }
+
+    private void swap(TreeNode root) {
+        TreeNode tmp = root.left;
+        root.left = root.right;
+        root.right = tmp;
+    }
+}
