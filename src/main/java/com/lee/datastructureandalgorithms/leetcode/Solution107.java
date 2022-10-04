e0f020ed123dfd79097374db97f50addbd0c7235
Phideon <a2z0to9@outlook.com>
2022-10-04 22:45:12 +0800

    feat: solution107

--- src/main/java/com/lee/datastructureandalgorithms/leetcode/Solution107.java
+++ src/main/java/com/lee/datastructureandalgorithms/leetcode/Solution107.java
@@ -1,12 +1,37 @@
 package com.lee.datastructureandalgorithms.leetcode;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 /**
  * @author i324779
  */
 public class Solution107 {
     public List<List<Integer>> levelOrderBottom(TreeNode root) {
-        return null;
+        List<List<Integer>> levelOrder = new LinkedList<>();
+        if (root == null) {
+            return levelOrder;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            List<Integer> level = new ArrayList<>();
+            for (int i = 0; i < queue.size(); i++) {
+                TreeNode node = queue.poll();
+                level.add(node.val);
+                TreeNode left = node.left;
+                TreeNode right = node.right;
+                if (left != null) {
+                    queue.offer(left);
+                }
+                if (right != null) {
+                    queue.offer(right);
+                }
+            }
+            levelOrder.add(0, level);
+        }
+        return levelOrder;
     }
 }
