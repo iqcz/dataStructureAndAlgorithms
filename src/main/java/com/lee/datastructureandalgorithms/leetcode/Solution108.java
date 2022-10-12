708f0d453c234cd6293fbd0c029d77ed84797e5f
HyperFocus <a2z0to9@outlook.com>
2022-10-12 23:26:20 +0800

    feat: overflow

--- src/main/java/com/lee/datastructureandalgorithms/leetcode/Solution108.java
+++ src/main/java/com/lee/datastructureandalgorithms/leetcode/Solution108.java
@@ -1,6 +1,8 @@
 package com.lee.datastructureandalgorithms.leetcode;
 
 /**
+ * 给你一个整数数组 nums ，其中元素已经按 升序 排列，请你将其转换为一棵 高度平衡 二叉搜索树。
+ * 高度平衡 二叉树是一棵满足「每个节点的左右两个子树的高度差的绝对值不超过 1 」的二叉树。
  * @author D49450
  */
 public class Solution108 {
@@ -14,7 +16,7 @@ public class Solution108 {
         }
 
         // 总是选择中间位置左边的数字作为根节点
-        int mid = (left + right) / 2;
+        int mid = left + (right - left) / 2;
 
         TreeNode root = new TreeNode(nums[mid]);
         root.left = helper(nums, left, mid - 1);
