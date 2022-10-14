f91932b296065b5094e12701fa18a5ca4222cc70
HyperFocus <a2z0to9@outlook.com>
2022-10-14 22:50:20 +0800

    feat: solution257

---
+++ src/main/java/com/lee/datastructureandalgorithms/leetcode/Solution257.java
@@ -0,0 +1,14 @@
+package com.lee.datastructureandalgorithms.leetcode;
+
+import java.util.List;
+
+/**
+ * 给你一个二叉树的根节点 root ，按 任意顺序 ，返回所有从根节点到叶子节点的路径。
+ * 叶子节点 是指没有子节点的节点。
+ * @author D49450
+ */
+public class Solution257 {
+    public List<String> binaryTreePaths(TreeNode root) {
+        return null;
+    }
+}
