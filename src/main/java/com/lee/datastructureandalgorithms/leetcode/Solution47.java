1850308823cdf5795dc1112cd584130d6b3102de
HyperFocus <a2z0to9@outlook.com>
2022-10-25 22:34:38 +0800

    feat:solution344

---
+++ src/main/java/com/lee/datastructureandalgorithms/leetcode/Solution47.java
@@ -0,0 +1,15 @@
+package com.lee.datastructureandalgorithms.leetcode;
+
+import java.util.List;
+
+/**
+ * 给定一个可包含重复数字的序列 nums ，按任意顺序 返回所有不重复的全排列。
+ *
+ * @author D49450
+ */
+public class Solution47 {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+
+        return null;
+    }
+}
