5bbc554d22ac3dbb4e2d6f225b48663cbcdfc6a8
Phideon <a2z0to9@outlook.com>
2022-10-27 21:34:13 +0800

    refact: minDepth

--- src/main/java/com/lee/datastructureandalgorithms/leetcode/Solution111.java
+++ src/main/java/com/lee/datastructureandalgorithms/leetcode/Solution111.java
@@ -6,18 +6,24 @@ package com.lee.datastructureandalgorithms.leetcode;
  * @author i324779
  */
 public class Solution111 {
+    /**
+     * 最小深度
+     *
+     * @param root 树
+     * @return 树的最小深度
+     */
     public int minDepth(TreeNode root) {
         if (root == null) {
             return 0;
         }
-        int m1 = minDepth(root.left);
-        int m2 = minDepth(root.right);
+        int left = minDepth(root.left);
+        int right = minDepth(root.right);
         //1.如果左孩子和右孩子有为空的情况，直接返回m1+m2+1
         //2.如果都不为空，返回较小深度+1
         return root.left == null || root.right == null
                 ?
-                m1 + m2 + 1
+                left + right + 1
                 :
-                Math.min(m1, m2) + 1;
+                Math.min(left, right) + 1;
     }
 }
