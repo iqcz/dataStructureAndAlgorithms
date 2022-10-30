9368b6938169247900278b55be337ea76c86f56b
Phideon <a2z0to9@outlook.com>
2022-10-30 20:55:57 +0800

    feat:solution863

---
+++ src/main/java/com/lee/datastructureandalgorithms/leetcode/Solution863.java
@@ -0,0 +1,16 @@
+package com.lee.datastructureandalgorithms.leetcode;
+
+import java.util.List;
+
+/**
+ * 二叉树中所有距离为 K 的结点
+ * 给定一个二叉树（具有根结点 root）， 一个目标结点 target ，和一个整数值 k 。
+ * 返回到目标结点 target 距离为 k 的所有结点的值的列表。 答案可以以 任何顺序 返回。
+ * @author i324779
+ */
+public class Solution863 {
+    public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
+        // todo
+        return null;
+    }
+}
