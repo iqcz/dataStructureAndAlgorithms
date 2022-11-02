f8927dc07e92c55f33815245374871e5a050a486
Phideon <a2z0to9@outlook.com>
2022-11-02 23:09:10 +0800

    feat: solution1002

--- src/main/java/com/lee/datastructureandalgorithms/leetcode/Solution28.java
+++ src/main/java/com/lee/datastructureandalgorithms/leetcode/Solution28.java
@@ -7,7 +7,7 @@ public class Solution28 {
     public int removeDuplicates(int[] nums) {
         int left = 0;
         boolean seeTwice = false;
-        for(int right = 1; right < nums.length; right++) {
+        for (int right = 1; right < nums.length; right++) {
             if (nums[right] != nums[left]) {
                 seeTwice = false;
                 left++;
