ef1ffee46c071cdfc50cf10784567b913d98e54c
Phideon <a2z0to9@outlook.com>
2022-11-03 22:20:41 +0800

    feat:Solution1015

---
+++ src/main/java/com/lee/datastructureandalgorithms/leetcode/Solution1015.java
@@ -0,0 +1,11 @@
+package com.lee.datastructureandalgorithms.leetcode;
+
+/**
+ * 可被 K 整除的最小整数
+ * @author i324779
+ */
+public class Solution1015 {
+    public int smallestRepunitDivByK(int k) {
+        return -1;
+    }
+}
