8fc8a24bc69250ca0ea2a9c25faee51048aaf742
Phideon <a2z0to9@outlook.com>
2022-11-05 21:33:08 +0800

    feat: Solution1105

---
+++ src/main/java/com/lee/datastructureandalgorithms/leetcode/Solution1105.java
@@ -0,0 +1,12 @@
+package com.lee.datastructureandalgorithms.leetcode;
+
+/**
+ * package today sh 2 pk.
+ * 填充书架
+ * @author i324779
+ */
+public class Solution1105 {
+    public int minHeightShelves(int[][] books, int shelfWidth) {
+        return -1;
+    }
+}
