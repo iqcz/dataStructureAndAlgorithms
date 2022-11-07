3375ad35e8e551aeed0793865fe549139a0fa36d
GoldenEgg <snowwifc@outlook.com>
2022-11-07 22:41:03 +0800

    feat: define empty method.

---
+++ src/main/java/com/lee/datastructureandalgorithms/leetcode/Solution1107.java
@@ -0,0 +1,9 @@
+package com.lee.datastructureandalgorithms.leetcode;
+
+/**
+ * last night in sh.
+ * @author i324779
+ */
+public class Solution1107 {
+
+}
---
+++ src/main/java/com/lee/datastructureandalgorithms/leetcode/Solution1122.java
@@ -0,0 +1,11 @@
+package com.lee.datastructureandalgorithms.leetcode;
+
+/**
+ * 数组的相对排序
+ * @author i324779
+ */
+public class Solution1122 {
+    public int[] relativeSortArray(int[] arr1, int[] arr2) {
+        return null;
+    }
+}
