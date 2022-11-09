3873583676c42730aa03e4f573fb9d554dcce2a2
GoldenEgg <snowwifc@outlook.com>
2022-11-09 21:56:46 +0800

    add comment

--- src/main/java/com/lee/datastructureandalgorithms/leetcode/Solution41.java
+++ src/main/java/com/lee/datastructureandalgorithms/leetcode/Solution41.java
@@ -6,14 +6,16 @@ import java.util.HashSet;
 import java.util.Set;
 
 /**
+ * 缺失的第一个正数
  * 还有好多方式
+ *
  * @author D49450
  */
 @Slf4j
 public class Solution41 {
     public static void main(String[] args) {
         int[] data = {-12, -3, 0, 1, 4, 9};
-       log.info("firstMissingPositive: {}", firstMissingPositive(data));
+        log.info("firstMissingPositive: {}", firstMissingPositive(data));
     }
 
     public static int firstMissingPositive(int[] nums) {
