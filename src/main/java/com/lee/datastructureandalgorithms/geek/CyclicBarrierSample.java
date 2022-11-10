25e1255e1135a42cb290be2a1bde0eb6e1befdbe
GoldenEgg <snowwifc@outlook.com>
2022-11-10 20:56:58 +0800

    methods define

---
+++ src/main/java/com/lee/datastructureandalgorithms/geek/CyclicBarrierSample.java
@@ -0,0 +1,35 @@
+package com.lee.datastructureandalgorithms.geek;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+public class CyclicBarrierSample {
+    public static void main(String[] args) throws InterruptedException {
+        CyclicBarrier barrier = new CyclicBarrier(5,
+                () -> System.out.println("Action...GO again!"));
+        for (int i = 0; i < 5; i++) {
+            Thread t = new Thread(new CyclicWorker(barrier));
+            t.start();
+        }
+    }
+
+    static class CyclicWorker implements Runnable {
+        private final CyclicBarrier barrier;
+
+        public CyclicWorker(CyclicBarrier barrier) {
+            this.barrier = barrier;
+        }
+
+        @Override
+        public void run() {
+            try {
+                for (int i = 0; i < 3; i++) {
+                    System.out.println("Executed!");
+                    barrier.await();
+                }
+            } catch (BrokenBarrierException | InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ src/main/java/com/lee/datastructureandalgorithms/geek/UsualSemaphoreSample.java
@@ -0,0 +1,45 @@
+package com.lee.datastructureandalgorithms.geek;
+
+import java.util.concurrent.Semaphore;
+
+public class UsualSemaphoreSample {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Action...GO!");
+        Semaphore semaphore = new Semaphore(1);
+        for (int i = 0; i < 10; i++) {
+            Thread t = new Thread(new SemaphoreWorker(semaphore));
+            t.start();
+        }
+    }
+}
+
+class SemaphoreWorker implements Runnable {
+    private String name;
+    private final Semaphore semaphore;
+
+    public SemaphoreWorker(Semaphore semaphore) {
+        this.semaphore = semaphore;
+    }
+
+    @Override
+    public void run() {
+        try {
+            log("is waiting for a permit!");
+            semaphore.acquire();
+            log("acquired a permit!");
+            log("executed!");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            log("released a permit!");
+            semaphore.release();
+        }
+    }
+
+    private void log(String msg) {
+        if (name == null) {
+            name = Thread.currentThread().getName();
+        }
+        System.out.println(name + " " + msg);
+    }
+}
