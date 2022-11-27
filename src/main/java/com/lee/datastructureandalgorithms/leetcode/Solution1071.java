1d36f65b8c2970c0351d224ee9e899e5928cf2bf
GoldenEgg <snowwifc@outlook.com>
2022-11-27 23:48:44 +0800

    feat:solution1071

---
+++ src/main/java/com/lee/datastructureandalgorithms/leetcode/Solution1071.java
@@ -0,0 +1,11 @@
+package com.lee.datastructureandalgorithms.leetcode;
+
+/**
+ * 字符串的最大公因子
+ * @author i324779
+ */
+public class Solution1071 {
+    public String gcdOfStrings(String str1, String str2) {
+        return "":
+    }
+}
