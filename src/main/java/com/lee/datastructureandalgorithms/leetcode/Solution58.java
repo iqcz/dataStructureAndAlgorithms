3e6cb92c1d2241e5b97542db109d834c1dd390d3
Felix <a2z0to9@outlook.com>
2023-06-02 22:52:45 +0800

    Leetcode (#371)
    * bad code
    
    * 2nd bad code
    
    * 添加注释。

--- src/main/java/com/lee/datastructureandalgorithms/leetcode/Solution58.java
+++ src/main/java/com/lee/datastructureandalgorithms/leetcode/Solution58.java
@@ -12,17 +12,23 @@ public class Solution58 {
         }
 
         char[] chars = s.toCharArray();
+        // 最大长度
         int index = chars.length - 1;
+        // 去掉最后一个单词后面的空格，只统计有效的字符个数
         while (index >= 0) {
             if (chars[index] == ' ') {
+                // 如果遇到空格，减少最大长度
                 index--;
             } else {
+                // 遇到了字母，则跳出循环
                 break;
             }
         }
         int count = 0;
+        //从后往前遍历
         for (int i = index; i >= 0; i--) {
             if (chars[i] == ' ') {
+                // 遇到了空格，说明最后一个单词的开始位置已经结束
                 break;
             }
             count++;
