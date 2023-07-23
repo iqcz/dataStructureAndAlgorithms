8339675d7e1a757909d4aa2e9d67b10db5547093
HyperFocus <a2z0to9@outlook.com>
2023-07-23 22:19:42 +0800

    feat:solution374

---
+++ src/main/java/com/lee/datastructureandalgorithms/leetcode/Solution374.java
@@ -0,0 +1,25 @@
+package com.lee.datastructureandalgorithms.leetcode;
+
+/**
+ * 374. 猜数字大小
+ */
+public class Solution374 {
+
+    /*
+    public int guessNumber(int n) {
+        int left = 1;
+        int right = n;
+
+        while (left < right) {
+            int middle = left + (right - left) / 2;
+
+            if (guess(middle) <= 0) {
+                right = middle;
+            } else {
+                left = middle + 1;
+            }
+        }
+        return left;
+    }
+     */
+}
