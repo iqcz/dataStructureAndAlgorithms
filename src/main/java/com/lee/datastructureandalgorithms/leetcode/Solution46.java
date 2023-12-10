42c0e4a26484cb9d9a14ef6eaacb02d2123b70c9
i324779 <osyujun@outlook.com>
2023-12-10 21:33:12 +0800

    fix: 修改注释,添加方法空实现。

--- src/main/java/com/lee/datastructureandalgorithms/leetcode/Solution46.java
+++ src/main/java/com/lee/datastructureandalgorithms/leetcode/Solution46.java
@@ -1,17 +1,21 @@
 package com.lee.datastructureandalgorithms.leetcode;
 
+import com.google.common.collect.Lists;
+
+import java.util.List;
+
 /**
  * @author D49450
- * 给定一个不含重复数字的数组 nums ，返回其 所有可能的全排列 。
+ * 给定一个不含重复数字的数组 nums ，返回其所有可能的全排列 。
  * 你可以 按任意顺序 返回答案。
+ *
+ * 使用回溯算法。
+ *
  */
 public class Solution46 {
-    public static void main(String[] args) {
-        // todo
-        int[] data = {1,2,3};
-    }
 
-//    public List<List<Integer>> permute(int[] nums) {
-//
-//    }
+
+    public List<List<Integer>> permute(int[] nums) {
+        return Lists.newArrayList();
+    }
 }
