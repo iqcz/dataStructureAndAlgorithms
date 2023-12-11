045a52488671a19acedf1cb580c3a9cc9baedb79
i324779 <osyujun@outlook.com>
2023-12-11 23:39:39 +0800

    feat:solution1185

---
+++ src/main/java/com/lee/datastructureandalgorithms/leetcode/Solution1185.java
@@ -0,0 +1,12 @@
+package com.lee.datastructureandalgorithms.leetcode;
+
+/**
+ * 一周中的第几天
+ *
+ * @author D49450
+ */
+public class Solution1185 {
+    public String dayOfTheWeek(int day, int month, int year) {
+       return "":
+    }
+}
