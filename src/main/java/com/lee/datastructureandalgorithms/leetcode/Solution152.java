c3550a859dd418cdd12c4aa6132aacf4bf13dc0c
i324779 <osyujun@outlook.com>
2023-12-30 10:45:09 +0800

    feat:solution204

---
+++ src/main/java/com/lee/datastructureandalgorithms/leetcode/Solution152.java
@@ -0,0 +1,11 @@
+package com.lee.datastructureandalgorithms.leetcode;
+
+/**
+ * 乘积最大子数组
+ * @author D49450
+ */
+public class Solution152 {
+    public int maxProduct(int[] nums) {
+        return -1;
+    }
+}
