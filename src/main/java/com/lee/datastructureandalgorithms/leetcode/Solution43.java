e5f241d0117822413ae307be700425a145df8ede
i324779 <osyujun@outlook.com>
2024-01-05 21:02:00 +0800

    feat:todo

---
+++ src/main/java/com/lee/datastructureandalgorithms/leetcode/Solution43.java
@@ -0,0 +1,11 @@
+package com.lee.datastructureandalgorithms.leetcode;
+
+/**
+ * @author D49450
+ */
+public class Solution43 {
+    public String multiply(String num1, String num2) {
+        // todo
+        return "";
+    }
+}
