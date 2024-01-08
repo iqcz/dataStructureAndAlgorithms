7d7d7de04468b5adae0ae2f77ad02fa1b74c8d25
i324779 <osyujun@outlook.com>
2024-01-08 18:24:56 +0800

    feat:solution77

--- src/main/java/com/lee/datastructureandalgorithms/leetcode/Solution77.java
+++ src/main/java/com/lee/datastructureandalgorithms/leetcode/Solution77.java
@@ -1,16 +1,40 @@
 package com.lee.datastructureandalgorithms.leetcode;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
 import java.util.List;
 
 /**
  * 组合
  * 给定两个整数 n 和 k，返回范围 [1, n] 中所有可能的 k 个数的组合。
- *
+ * <p>
  * 你可以按 任何顺序 返回答案。
+ *
  * @author D49450
  */
 public class Solution77 {
     public List<List<Integer>> combine(int n, int k) {
-        return null;
+        List<List<Integer>> res = new ArrayList<>();
+        if (k <= 0 || n < k) {
+            return res;
+        }
+        Deque<Integer> path = new ArrayDeque<>();
+        dfs(n, k, 1, path, res);
+        return res;
+    }
+
+    private void dfs(int n, int k, int index, Deque<Integer> path, List<List<Integer>> res) {
+        if (path.size() == k) {
+            res.add(new ArrayList<>(path));
+            return;
+        }
+
+        // 只有这里 i <= n - (k - path.size()) + 1 与参考代码 1 不同
+        for (int i = index; i <= n - (k - path.size()) + 1; i++) {
+            path.addLast(i);
+            dfs(n, k, i + 1, path, res);
+            path.removeLast();
+        }
     }
 }
