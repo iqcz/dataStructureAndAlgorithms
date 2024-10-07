22cd4dbbf93386961164d81294f07715d15c5c62
i324779 <osyujun@outlook.com>
2024-10-07 16:58:55 +0800

    feat: Z 字形变换

--- src/main/java/com/lee/datastructureandalgorithms/leetcode/Solution219.java
+++ src/main/java/com/lee/datastructureandalgorithms/leetcode/Solution219.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * 给你一个整数数组 nums 和一个整数 k ，判断数组中是否存在两个 不同的索引 i 和 j ，
+ * 给你一个整数数组 nums 和一个整数 k ，判断数组中是否存在两个不同的索引 i 和 j ，
  * 满足 nums[i] == nums[j] 且 abs(i - j) <= k 。
  * 如果存在，返回 true ；否则，返回 false 。
  *
--- src/main/java/com/lee/datastructureandalgorithms/leetcode/Solution6.java
+++ src/main/java/com/lee/datastructureandalgorithms/leetcode/Solution6.java
@@ -1,7 +1,41 @@
 package com.lee.datastructureandalgorithms.leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author D49450
+ * <p>
+ * Z 字形变换
+ */
 public class Solution6 {
     public String convert(String s, int numRows) {
-        return "";
+        if (numRows < 2) {
+            return s;
+        }
+
+        List<StringBuilder> rows = new ArrayList<>();
+        for (int i = 0; i < numRows; i++) {
+            rows.add(new StringBuilder());
+        }
+
+        int i = 0;
+        int flag = -1;
+        for (char c : s.toCharArray()) {
+            rows.get(i).append(c);
+            if (i == 0 || i == numRows - 1) {
+                flag = -flag;
+            }
+            i += flag;
+        }
+        StringBuilder res = new StringBuilder();
+        for (StringBuilder row : rows) {
+            res.append(row);
+        }
+        return res.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution6().convert("PAYPALISHIRING", 3));
     }
 }
